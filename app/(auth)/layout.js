4ee57d25e56eda140911a8f0c06c1da3dca8deb6
Onkar Bandichhode <onkarbandichhode@gmail.com>
2026-01-18 03:06:45 +0530

    first commit

---
+++ app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({children}) => {
+  return (
+    <div className='flex items-center justify-center h-screen'>{children}</div>
+  )
+}
+
+export default Layout
